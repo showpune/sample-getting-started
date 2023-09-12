8fc9177c43a5ad575e2278e3a2b41542bed7184c
zhiyongli <iamlizhiyong@outlook.com>
2023-09-12 10:58:21 +0800

    Add server config

---
+++ asa-e.sh
@@ -0,0 +1,2 @@
+az account set -s 0753feba-86f1-4242-aff1-27938fb04531
+az spring app deploy -s zhiyongli-asc-e -g zhiyongli -n openliberity --source-path . --build-env BP_JAVA_APP_SERVER=liberty BP_MAVEN_BUILT_ARTIFACT="target/*.[ejw]ar src/main/liberty/config/*"
